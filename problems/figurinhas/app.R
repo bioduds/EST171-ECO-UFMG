355ef2f477aae6b04572bb16dcd45bf31047178d
bioduds <bioduds@gmail.com>
2020-12-06 22:36:49 +0000

    Melhorando Figurinhas

--- problems/figurinhas/app.R
+++ problems/figurinhas/app.R
@@ -1,6 +1,37 @@
 ## Eduardo Capanema
 ## eduardocapanema@ufmg.br
 
+library( plotly )
+
+S = c( 3, 30, 300 )
+N = 100
+allMedias = c()
+
+for( band in length(S) ) {
+    allDraws = c()
+    for( i in 1:S[band] ) {
+        draws = 0
+        album = c()
+        while( length( album ) < N ) {
+            draws <- draws + 1
+            r1 <- sample( 1:N, 1 )
+            album <- append( album, r1 )
+            album <- unique( album );
+        }
+        allDraws <- append( allDraws, draws )
+    }
+    allMedias <- append( allMedias, allDraws )
+}
+
+
+fig <- plot_ly( data = iris, x = 1:length(allMedias), y = allMedias,
+                marker = list( size=length(allMedias), color='rgba(255, 182, 193, .9)', line=list( color='rgba(152, 0, 0, .8)', width=2 ) ) )
+fig <- fig %>% layout( title = 'Compras de Figurinhas',
+         yaxis = list( zeroline = FALSE ),
+         xaxis = list( zeroline = FALSE ) )
+
+fig
+
 library( dash )
 library( dashHtmlComponents )
 library( dashCoreComponents )
@@ -8,70 +39,12 @@ library( dashCoreComponents )
 app <- Dash$new()
 
 app$layout(
-  htmlDiv(
-    list(
-      dccInput( id="graphTitle", value="Figurinhasss", type="text" ),
-      htmlDiv( id="outputID" ),
-      dccGraph( id="plota",
-                figure = list(
-                    data = list( x=c( 1, 2, 3 ), y=c( 3, 2, 8 ), type="scatter" ),
-                    layout = list( title="Let's Simulate!" )
+    htmlDiv(
+        list(
+            htmlDiv( allMedias ),
+            dccGraph( figure=fig ) 
         )
-      )
     )
-  )
 )
 
-app$callback( output=list( id="plota", property="figure" ),
-              params=list( input( "graphTitle", "value" ) ),
-              function( newTitle ) {
-                 rand1 <- sample( 1:100, 1 )
-                 rand2 <- sample( 1:100, 1 )
-                 rand3 <- sample( 1:100, 1 )
-                 rand4 <- sample( 1:100, 1 )
-                 rand5 <- sample( 1:100, 1 )
-                 rand6 <- sample( 1:100, 1 )
-                 x <- c( 1, 2, 3 )
-                 y <- c( rand1, rand2, rand3 )
-                 y2 <- c( rand4, rand5, rand6 )
-                 df = data.frame( x, y, y2 )
-                 list(
-                   data =
-                     list(
-                       list( x=df$x, y=df$y, type="box" ),
-                       list( x=df$x, y=df$y2, type="box" )
-                     ),
-                   layout = list( title=newTitle )
-                 )
-               }
-)
-
-app$callback( output=list( id="outputID", property="children" ),
-              params = list( input( "graphTitle", "value" ), state( "graphTitle", "type" ) ),
-              function( x, y ) {
-
-                ##############################################################################################
-                S = 3000
-                N = 25
-                allDraws = c()
-                for( i in 1:S ) {
-                    #print( paste( "Simulação", i ) )
-                    draws = 0
-                    album = c()
-                    while( length( album ) < N ) {
-                        draws <- draws + 1
-                        r1 <- sample( 1:N, 1 )
-                        album <- append( album, r1 )
-                        album <- unique( album );
-                    }
-                    allDraws <- append( allDraws, draws )
-                }
-                resp = round( mean( allDraws ) )
-                ##############################################################################################
-
-                sprintf( "Numero de Compras de pacotes com uma figurinha: %s", resp )
-
-             }
-)
-
-app$run_server( showcase=FALSE )
+app$run_server( debug=TRUE, dev_tools_hot_reload=TRUE, showcase=FALSE )
